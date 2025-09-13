997b28503c431966c7f30cbde7405990cee78e4d
ADNILSON DE PAULA DOS SANTOS <133922383+Adnilsondp@users.noreply.github.com>
2025-09-13 15:57:25 -0300

    A soma
    A soma é:

---
+++ main.py
@@ -0,0 +1,12 @@
+# soma_tres.py
+
+# pede três números para o usuário
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+num3 = float(input("Digite o terceiro número: "))
+
+# faz a soma
+resultado = num1 + num2 + num3
+
+# mostra o resultado
+print(f"A soma de {num1} + {num2} + {num3} é {resultado}")
